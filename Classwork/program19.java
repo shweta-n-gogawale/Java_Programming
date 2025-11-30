a4e5d67d23493f97a3b2267b987a57346bc79e20
Shweta Gogawale <gogawaleshweta12@gmail.com>
2025-11-30 10:06:20 +0530

    Added all Classwork Java programs

---
+++ Classwork/program19.java
@@ -0,0 +1,17 @@
+class program19 
+{
+    public void Display()
+    {
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+    }
+
+    public static void main(String A[]) 
+    {
+        Display();  //ERROR:non-static method Display() cannot be referenced from a static context
+
+    }
+    
+}
